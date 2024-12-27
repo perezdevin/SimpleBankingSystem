22a9d6ca8fdd2c1dc50d2920205e09122a6014ee
Flor Martinez <flormartinez@MacBook-Pro-de-Flor.local>
2024-12-27 16:29:27 +0100

    modified main

--- SimpleBankingSystem/src/SimpleBankingSystem/Main.java
+++ SimpleBankingSystem/src/SimpleBankingSystem/Main.java
@@ -5,24 +5,24 @@ package SimpleBankingSystem;
 */
 public class Main {
 	 public static void main(String[] args) {
-		 Account myAccount;
+		Account myAccount;
 	        
-	     myAccount = new Account("Flor Martinez", "1000-1234-56-123456789", 2500);
+	    myAccount = new Account("Flor Martinez", "1000-1234-56-123456789", 2500);
 	     
-	     try {
-        	myAccount.withdrawAmount(2300);
-	     } catch(Exception e){
+	    try {
+	    	myAccount.withdrawAmount(2300);
+	    } catch(Exception e){
         	System.err.println(e.getMessage());
             System.out.println("Error al retirar");
-	     }
+	    }
 	     
-	     try {
+	    try {
 	    	 System.out.println("Ingrés al compte");
 	    	 myAccount.depositAmount(1695);
-	     } catch(Exception e){
+	    } catch(Exception e){
 	    	 System.err.println(e.getMessage());
 	         System.out.println("Error en l'ingrés");
-	     }
+	    }
 
 	     System.out.println("El saldo actual es " + myAccount.getBalance());   
 	 }
